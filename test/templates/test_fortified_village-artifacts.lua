67a2f6bdf8b323821b5ceb9d2a34848a570a7f88
Simon Heimberg <simon.heimberg@heimberg-ea.ch>
2024-01-15 16:44:43 +0300

    test_fortified_village-artifacts.lua conquers artifacts independent of map

--- test/templates/test_fortified_village-artifacts.lua
+++ test/templates/test_fortified_village-artifacts.lua
@@ -1,24 +1,32 @@
 include "test/scripting/check_game_end.lua"
 
 local winner = 1
-local artifacts = {
-  { 68, 29 },
-  { 85, 41 },
-  { 40, 72 },
-  { 36, 82 },
-}
 
 run(function()
   sleep(8000)
 
   -- conquer the artifacts
-  for i,coords in pairs(artifacts) do
-    sleep(2000)
-    print(string.bformat("Conquering artifact at %d,%d for the winner.", coords[1], coords[2]))
-    game.players[winner]:conquer(game.map:get_field(coords[1],coords[2]), 3)
-  end
+   local n_artifacts = 0
+   local map = wl.Game().map
+   for x=0, map.width-1 do
+      for y=0, map.height-1 do
+         local field = map:get_field(x,y)
+         if field.immovable and field.immovable:has_attribute("artifact") and
+               field.immovable.fields[1] == field then
+            n_artifacts = n_artifacts + 1
+            sleep(2000)
+            print(string.bformat("Conquering artifact at %d,%d for the winner.", field.x, field.y))
+            game.players[winner]:conquer(field)
+         end
+      end
+   end
 
-  print("All artifacts are conquered for the winner, win condition should be triggered soon.")
+   if n_artifacts == 0 then
+      sleep(2000)
+      assert(false, "No artifacts on the map. The win condition artifacts will not end.")
+   else
+      print("All artifacts are conquered for the winner, win condition should be triggered soon.")
+   end
 end)
 
 check_win_condition(winner)
