3e19bc4b6c748615ac8b07ebf5fde13f693bd64a
Tóth András <txa-dev@posteo.hu>
2024-05-14 12:26:26 +0200

    try to fix msvc and macos, re-add gcc on ubuntu 20.04

--- cmake/WlFunctions.cmake
+++ cmake/WlFunctions.cmake
@@ -182,7 +182,9 @@ macro(_common_compile_tasks)
   endif()
 
   if(ARG_USES_STD_FS)
-    target_link_libraries(${NAME} ${TARGET_LINK_FLAGS} stdc++fs)
+    if (NOT MSVC AND NOT APPLE)
+      target_link_libraries(${NAME} ${TARGET_LINK_FLAGS} stdc++fs)
+    endif()
   endif()
 
   if(ARG_USES_TINYGETTEXT)
