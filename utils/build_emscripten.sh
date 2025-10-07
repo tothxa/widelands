621c8a034ee6e3e20364b1d0efe71692f7118429
Tóth András <txa-dev@posteo.hu>
2025-10-07 22:30:36 +0200

    make git trust parent dir

--- utils/build_emscripten.sh
+++ utils/build_emscripten.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+git config --global --add safe.directory /src
+
 mkdir build_emscripten
 cd build_emscripten
 emcmake cmake .. -DCMAKE_BUILD_PLATFORM=emscripten -DCMAKE_BUILD_TYPE=Release -DOPTION_BUILD_WEBSITE_TOOLS="OFF" -DOPTION_ASAN="OFF" -DOPTION_BUILD_CODECHECK="OFF" -DCMAKE_EXE_LINKER_FLAGS="-sMODULARIZE -sEXPORTED_RUNTIME_METHODS=callMain -sEXPORT_ALL -sEXPORT_NAME=widelands" -DOPTION_BUILD_TESTS=OFF -DOPTION_BUILD_WEBSITE_TOOLS=OFF
