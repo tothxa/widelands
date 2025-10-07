2454b514cb3d34de94d206cbd7e1046b3fdd1b9d
Tóth András <txa-dev@posteo.hu>
2025-10-07 22:24:21 +0200

    no fixed version, don't use pushd in sh

--- utils/build_emscripten.sh
+++ utils/build_emscripten.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
+set -e
+
 mkdir build_emscripten
-pushd build_emscripten
+cd build_emscripten
 emcmake cmake .. -DCMAKE_BUILD_PLATFORM=emscripten -DCMAKE_BUILD_TYPE=Release -DOPTION_BUILD_WEBSITE_TOOLS="OFF" -DOPTION_ASAN="OFF" -DOPTION_BUILD_CODECHECK="OFF" -DCMAKE_EXE_LINKER_FLAGS="-sMODULARIZE -sEXPORTED_RUNTIME_METHODS=callMain -sEXPORT_ALL -sEXPORT_NAME=widelands" -DOPTION_BUILD_TESTS=OFF -DOPTION_BUILD_WEBSITE_TOOLS=OFF
 grep -v REVDETECT-BROKEN VERSION
 emmake make -k -j$(nproc) VERBOSE=1
